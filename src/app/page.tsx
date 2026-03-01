5c9eb49cc0c9e3ae81b1830777a1d356e08362e4
dearvirussir <dear.virus.420@gmail.com>
2026-03-01 14:28:02 +0500

    Reordered Gallery (Our Work in Action) to appear before Urgent Cases

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,10 +22,8 @@ export default function Home() {
     <ScrollRevealWrapper>
       <main className="min-h-screen">
         <Navbar />
-        <Hero />
-        <SpecialAppeal />
-        <Cases />
         <Gallery />
+        <Cases />
         <About />
         <Donation />
         <KeyInitiatives />
